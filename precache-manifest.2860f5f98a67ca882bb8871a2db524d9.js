cb08b794605bbbc873218ef96f2574dcf67a33c8
calinzabalt <calinzabalt@gmail.com>
2018-12-02 17:47:52 +0200

    Updates

---
+++ precache-manifest.2860f5f98a67ca882bb8871a2db524d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0c1f8d1e35cfc77d4c2",
+    "url": "/reminder/static/js/runtime~main.a0c1f8d1.js"
+  },
+  {
+    "revision": "e4b2e51885d744117fee",
+    "url": "/reminder/static/js/main.e4b2e518.chunk.js"
+  },
+  {
+    "revision": "5f15a3153f1b19a47249",
+    "url": "/reminder/static/js/1.5f15a315.chunk.js"
+  },
+  {
+    "revision": "e4b2e51885d744117fee",
+    "url": "/reminder/static/css/main.409a602c.chunk.css"
+  },
+  {
+    "revision": "b94a314e5cb92fb2e01de2e5c8d6e438",
+    "url": "/reminder/index.html"
+  }
+];
